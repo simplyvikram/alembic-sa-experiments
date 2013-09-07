e1863a1c68c00d4851549f67096d6260502307b7
vikram singh <simplyvikram@gmail.com>
2013-09-07 19:47:26 -0400

    added schema for ordering system

--- application/models.py
+++ application/models.py
@@ -1,9 +1,13 @@
-
 from sqlalchemy import (
     Column,
+    Float,
+    ForeignKey,
     Integer,
     String
 )
+
+from sqlalchemy.orm import backref, relationship
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -53,57 +57,92 @@ class FirstDeclarative(Base):
     value = Column(String(50))
 
 
-# class Customer(Base):
-#     __tablename__ = "customer"
-#
-#     customer_id = Column(Integer, primary_key=True)
-#     name = Column(String(50))
-#     phone_number = Column(String(20))
-#
-#     def __init__(self, name, phone_number):
-#         self.name = name
-#         self.phone_number = phone_number
-#
-#
-#     # may reference an array of orders
-#
-#
-# class Order(Base):
-#     __tablename__ = "order"
-#
-#     order_id = Column(Integer, primary_key=True)
-#     customer_id = Column(Integer)
-#
-#     def __init__(self, customer_id):
-#         self.customer_id = customer_id
-# #   includes an array of order_items
-#
-#
-# class Item(Base):
-#     __tablename__ = "item"
-#
-#     item_id = Column(Integer, primary_key=True)
-#     name = Column(String(40))
-#     description = Column(String(200))
-#     unit_price = Column(Float(2))
-#
-#     def __init__(self, name, description, unit_price):
-#         self.name = name
-#         self.description = description
-#         self.unit_price = unit_price
-#
-#
-# class OrderItem(Base):
-#     __tablename__ = "order_item"
-#
-#     order_item_id = Column(Integer, primary_key=True)
-#     order_id = Column(Integer)
-#     item_id = Column(Integer)
-#     item_quantity = Column(Integer)
-#     special_note = Column(String(200))
-#
-#     def __init__(self, order_id, item_id, item_quantity, special_note):
-#         self.order_id = order_id
-#         self.item_id = item_id
-#         self.item_quantity = item_quantity
-#         self.special_note = special_note
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    phone_number = Column(String(20), nullable=False)
+
+    orders = relationship("Order", backref=backref("customer"),
+                          cascade="all, delete, delete-orphan",
+                          passive_deletes=True,
+                          order_by="Order.id")
+
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+        self.orders = []
+
+        self.id = None
+
+    def __repr__(self):
+        return "<Customer -  id:%s, name:%s, orders:%s>" % \
+               (str(self.id), self.name, self.orders)
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+
+    order_items = relationship("OrderItem",
+                               cascade="all, delete, delete-orphan")
+
+    def __init__(self):
+        self.customer = None
+        self.order_items = []
+
+        self.id = None
+
+    def __repr__(self):
+        return "<Order - id:%s, customer_id:%s, customer_name:%s>" % \
+               (str(self.id), str(self.customer.id), self.customer.name)
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False)
+    description = Column(String(200), nullable=True)
+    unit_price = Column(Float(2), nullable=True)
+
+    order_items = relationship("OrderItem")
+
+    def __init__(self, name, unit_price, description=None):
+        self.name = name
+        self.unit_price = unit_price
+        self.description = description
+
+        self.id = None
+        self.order_items = []
+
+    def __repr__(self):
+        return "<Item - id:%s, name:%s, unit_price:%s>" % \
+               (str(self.id), self.name, str(self.unit_price))
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('orders.id'))
+    item_id = Column(Integer, ForeignKey('items.id'))
+    quantity = Column(Integer)
+    special_note = Column(String(200))
+
+    def __init__(self, item_id, quantity, special_note=None):
+        # self.order_id = order_id
+        self.item_id = item_id
+        self.quantity = quantity
+        self.special_note = special_note
+
+        self.id = None
+        self.order_id = None
+
+    def __repr__(self):
+        return "<OrderItem - id:%s, order_id:%s, item_id:%s, quantity:%d>" % \
+               (str(self.id), str(self.order_id),
+                str(self.item_id), self.quantity)
